1c80046957e6f5f4a0a430c75277e7c9eb23b1a3
Guiideed <guilhermenovolg@gmail.com>
2023-08-21 13:47:51 -0300

    funcoes java

---
+++ funcoes/script.js
@@ -0,0 +1,14 @@
+let a= 10;
+let b = 20;
+
+function funcao(a, b){
+    console.log(a+b);
+}
+funcao(a,b);
+
+
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo);
+
+}
+mensagem("Olá guilherme", "albano");
